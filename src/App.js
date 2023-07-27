5b602bcd8febbdd2bb43a1ad58139610835d4e8a
vivienphang <vivien.phang@lionsbot.com>
2023-07-27 23:52:27 +0800

    installed react-router-dom and added NavLayout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Landing from "./pages/Landing"
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import NavLayout from "./NavLayout";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<NavLayout />}>
+          <Route index element={<Landing />} />
+          {/* <Route path="edit" element={<EditUser  userToken={userToken}/>} /> */}
+        </Route>
+        <Route path="*" element={<Landing />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/NavLayout.jsx
@@ -0,0 +1,5 @@
+const NavLayout = () => {
+    return ( <h3>Navbar here.</h3> );
+}
+ 
+export default NavLayout;
