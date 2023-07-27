8747ab8fe81c34d466694f73c39eaede579d6bfb
vivienphang <vivien.phang@lionsbot.com>
2023-07-28 00:18:06 +0800

    feat (NavLayout): completed the UI for Nav

--- src/NavLayout.jsx
+++ src/NavLayout.jsx
@@ -1,5 +1,74 @@
+import { Link, Outlet } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import { Grid, AppBar, Box, Toolbar } from "@mui/material";
+
 const NavLayout = () => {
-    return ( <h3>Navbar here.</h3> );
-}
- 
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Grid item xs={9} container spacing={1}>
+              <Link to="/" style={{textDecoration: "none"}}>
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  variant="h5"
+                  color="white"
+            
+                >
+                  Booking App
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid item xs={2} display="flex" justifyContent="space-evenly">
+              <IconButton
+                size="small"
+                sx={{ p: 1 }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  variant="h6"
+                  color="white"
+                >
+                  Sign Up
+                </Typography>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        marginTop={10}
+      >
+        <Grid item xs={9}>
+          <Outlet />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
 export default NavLayout;
