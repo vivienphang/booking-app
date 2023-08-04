5fea043ed4a718449bfb360df5a9746905c0ad8d
vivienphang <vivienphang@gmail.com>
2023-08-04 17:41:48 +0800

    feat (bookingForm): setup booking UI

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,5 +1,97 @@
-const BookingForm = () => {
-    return ( <div style={{textAlign:"center"}}><h1>Booking Form.</h1></div> );
-}
- 
-export default BookingForm
+import { Grid, TextField, Typography, Button } from "@mui/material";
+import { getRoomDataById } from "../auth/firebase";
+import { useEffect } from "react";
+import styles from "./bookingForm.module.css";
+
+const BookingForm = ({ bookingData, setBookingData, roomId }) => {
+  const fetchData = async () => {
+    const data = await getRoomDataById(roomId);
+    setBookingData(data);
+    console.log("Data in booking form - ", data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  return (
+    <>
+      <Typography gutterBottom textAlign="center" marginBottom={6}>
+        <span className={styles.bookingHeader}>Booking Form</span>
+      </Typography>
+      <Typography gutterBottom textAlign="center" marginBottom={5}>
+        <span className={styles.roomKey}>Room: </span>
+        <span className={styles.roomValue}>{bookingData.name}</span>
+      </Typography>
+      <form>
+        <Grid container spacing={3} textAlign="center" justifyContent="center">
+          <Grid item xs={12}>
+            <TextField
+              label="Title"
+              placeholder="Add a title"
+              type="text"
+              name="title"
+              value={bookingData.date}
+              // onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Date"
+              type="date"
+              name="date"
+              value={bookingData.date}
+              // onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Start Time"
+              type="time"
+              name="startTime"
+              // value={bookingData.startTime}
+              // onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="End Time"
+              type="time"
+              name="endTime"
+              // value={bookingData.endTime}
+              // onChange={handleChange}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary">
+              Book Meeting Room
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  );
+};
+
+export default BookingForm;
--- src/pages/ViewAvailability.jsx
+++ src/pages/ViewAvailability.jsx
@@ -1,16 +1,34 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction"
 import { Grid } from "@mui/material";
 import BookingForm from '../components/BookingForm'
+import styles from "./calendar.module.css"
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ViewAvailability = () => {
+    const [bookingData, setBookingData] = useState({
+        title: "",
+        date: "",
+        startTime: "",
+        endTime: "",
+        roomId: ""
+    })
+    let { id } = useParams();
+    
+    const handleDateClick = (arg) => {
+        console.log(arg.dateStr)
+    }
+
   return (
     <Grid container spacing={1} p={3}>
-        <Grid item xs={8} >
-            <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        <Grid item xs={8} className={styles.CalendarView} >
+            <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} initialView="dayGridMonth"
+            dateClick={handleDateClick} />
         </Grid>
         <Grid item xs={4}>
-            <BookingForm />
+            <BookingForm setBookingData={setBookingData} bookingData={bookingData} roomId={id}/>
         </Grid>
     </Grid>
   );
