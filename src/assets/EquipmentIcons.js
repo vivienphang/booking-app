5f40a260173f31aa6e6a596c8e4c1580f2006fdd
vivienphang <vivienphang@gmail.com>
2023-08-11 11:18:22 +0800

    feat (RoomDisplay): added icons for each equipment

--- src/assets/EquipmentIcons.js
+++ src/assets/EquipmentIcons.js
@@ -2,6 +2,11 @@ import MonitorIcon from "@mui/icons-material/Monitor";
 import MicrophoneIcon from "@mui/icons-material/KeyboardVoice";
 import WifiIcon from "@mui/icons-material/Wifi";
 import PowerAdaptorIcon from "@mui/icons-material/ElectricalServices";
+import CoffeeMakerIcon from '@mui/icons-material/CoffeeMaker';
+import SpeakerRoundedIcon from '@mui/icons-material/SpeakerRounded';
+import VideocamOutlinedIcon from '@mui/icons-material/VideocamOutlined';
+import RectangleOutlinedIcon from '@mui/icons-material/RectangleOutlined';
+
 
 export const equipmentIcons = {
   "35\" TV monitor ": MonitorIcon,
@@ -9,6 +14,10 @@ export const equipmentIcons = {
   Microphone: MicrophoneIcon,
   Wifi: WifiIcon,
   "Power adaptor": PowerAdaptorIcon,
-  // Add more equipment names and icons as needed
+  "Coffee machine": CoffeeMakerIcon,
+  Speaker: SpeakerRoundedIcon,
+  Projector: VideocamOutlinedIcon,
+  Whiteboard: RectangleOutlinedIcon,
+
 };
 
